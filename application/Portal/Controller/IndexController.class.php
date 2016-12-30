81037ed01bb2c6868ffd8b7bcd2bf6a645fccf30
liuzufa <540015488@qq.com>
2016-12-30 19:53:49 +0800

    删除blog模块

--- application/Portal/Controller/IndexController.class.php
+++ application/Portal/Controller/IndexController.class.php
@@ -1,38 +1,66 @@
 <?php
-/*
- *      _______ _     _       _     _____ __  __ ______
- *     |__   __| |   (_)     | |   / ____|  \/  |  ____|
- *        | |  | |__  _ _ __ | | _| |    | \  / | |__
- *        | |  | '_ \| | '_ \| |/ / |    | |\/| |  __|
- *        | |  | | | | | | | |   <| |____| |  | | |
- *        |_|  |_| |_|_|_| |_|_|\_\\_____|_|  |_|_|
- */
-/*
- *     _________  ___  ___  ___  ________   ___  __    ________  _____ ______   ________
- *    |\___   ___\\  \|\  \|\  \|\   ___  \|\  \|\  \ |\   ____\|\   _ \  _   \|\  _____\
- *    \|___ \  \_\ \  \\\  \ \  \ \  \\ \  \ \  \/  /|\ \  \___|\ \  \\\__\ \  \ \  \__/
- *         \ \  \ \ \   __  \ \  \ \  \\ \  \ \   ___  \ \  \    \ \  \\|__| \  \ \   __\
- *          \ \  \ \ \  \ \  \ \  \ \  \\ \  \ \  \\ \  \ \  \____\ \  \    \ \  \ \  \_|
- *           \ \__\ \ \__\ \__\ \__\ \__\\ \__\ \__\\ \__\ \_______\ \__\    \ \__\ \__\
- *            \|__|  \|__|\|__|\|__|\|__| \|__|\|__| \|__|\|_______|\|__|     \|__|\|__|
- */
-// +----------------------------------------------------------------------
-// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2013-2014 http://www.thinkcmf.com All rights reserved.
-// +----------------------------------------------------------------------
-// | Author: Dean <zxxjjforever@163.com>
-// +----------------------------------------------------------------------
+
 namespace Portal\Controller;
 use Common\Controller\HomebaseController; 
-/**
- * 首页
- */
+
 class IndexController extends HomebaseController {
-	
-    //首页 小夏是老猫除外最帅的男人了
-	public function index() {
-    	$this->display(":index");
+
+
+    // 前台文章列表
+    public function index() {
+        $term_id=I('get.id',1,'intval');
+
+        if($term_id){
+            $term=sp_get_term($term_id);
+            $term[$term['term_id']]=$term;
+        }else{
+            $terms=sp_get_terms();
+            $term = [];
+            $term_ids = [];
+            foreach($terms as $value){
+                $term_ids[]=$value['term_id'];
+                $term[$value['term_id']] = $value;
+            }
+            $term_id = implode(',',$term_ids);
+        }
+
+
+
+        if(empty($term)){
+            header('HTTP/1.1 404 Not Found');
+            header('Status:404 Not Found');
+            if(sp_template_file_exists(MODULE_NAME."/404")){
+                $this->display(":404");
+            }
+            return;
+        }
+
+        $tplname= isset($term["list_tpl"])?$term["list_tpl"]:'index';
+        $tplname=sp_get_apphome_tpl($tplname, "index");
+        $this->assign('term',$term);
+        $this->assign('cat_id', $term_id);
+        $this->display(":index");
+    }
+
+    // 文章分类列表接口,返回文章分类列表,用于后台导航编辑添加
+    public function nav_index(){
+        $navcatname="文章分类";
+        $term_obj= M("Terms");
+
+        $where=array();
+        $where['status'] = array('eq',1);
+        $terms=$term_obj->field('term_id,name,parent')->where($where)->order('term_id')->select();
+        $datas=$terms;
+        $navrule = array(
+            "id"=>'term_id',
+            "action" => "Portal/List/index",
+            "param" => array(
+                "id" => "term_id"
+            ),
+            "label" => "name",
+            "parentid"=>'parent'
+        );
+        return sp_get_nav4admin($navcatname,$datas,$navrule) ;
     }
 
 }
